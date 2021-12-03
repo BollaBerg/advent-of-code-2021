b6d61fed24c643081522b747cbd4ca08b4eaaa7f
Andreas Berg <berg.andreasb@gmail.com>
2021-12-03 10:39:34 +0100

    Solve day 1, task 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,10 +1,18 @@
 use std::fs;
 
 /// Count times the value of entries increases
-fn count_depth_increase(entries: Vec<u16>) -> usize {
+fn count_depth_increase(entries: &Vec<u16>) -> usize {
     entries.windows(2)
         .filter(|pair| pair[1] > pair[0])
         .count()
+}
+
+/// Count times the sum of 3-width sliding windows increases
+fn count_sliding_depth_increase(entries: &Vec<u16>) -> usize {
+    let windows = entries.windows(3)
+        .map(|entry| entry.iter().sum())
+        .collect();
+    count_depth_increase(&windows)
 }
 
 fn main() {
@@ -18,9 +26,13 @@ fn main() {
         .collect();
     
     // Task 1
-    let depth_increases = count_depth_increase(entries_num);
-
-    println!("Times depth increases: {}", depth_increases)
+    let depth_increases = count_depth_increase(&entries_num);
+    println!("Times depth increases: {}", depth_increases);
 
     // Task 2
+    let sliding_depth_increases = 
+        count_sliding_depth_increase(&entries_num);
+    println!(
+        "Times sliding depth increases: {}",sliding_depth_increases
+    );
 }

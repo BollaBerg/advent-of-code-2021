bcbcd4aacfeb14c780eef0199c3c62720d7f6d1c
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2021-12-06 15:30:25 +0100

    Factor out some code to own function

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,15 +1,4 @@
-fn main() {
-    let entries = aoc_lib::read_entries("day3.txt");
-
-    let total_count = entries.lines().count() as u32;
-    let mut counts = vec![0; 12];
-
-    for line in entries.lines() {
-        for (i, value) in line.chars().enumerate() {
-            counts[i] += value.to_digit(2).unwrap();
-        }
-    }
-
+fn task1(counts : Vec<u32>, total_count : u32) {
     let mut gamma = 0;
     let mut epsilon = 0;
     let factor : i32 = 2;
@@ -26,4 +15,23 @@ fn main() {
     println!("Value of gamma  : {}", gamma);
     println!("Value of epsilon: {}", epsilon);
     println!("Multiplied value: {}", gamma * epsilon);
+}
+
+fn main() {
+    let entries = aoc_lib::read_entries("day3.txt");
+
+    let total_count = entries.lines().count() as u32;
+    let mut counts = vec![0; 12];
+
+    for line in entries.lines() {
+        for (i, value) in line.chars().enumerate() {
+            counts[i] += value.to_digit(2).unwrap();
+        }
+    }
+
+    // Task 1
+    task1(counts, total_count);
+
+    // Task 2
+
 }

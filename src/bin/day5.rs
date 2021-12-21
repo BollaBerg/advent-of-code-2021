efea4b327e2d34adf52725e699ee3f933e7c022d
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2021-12-21 15:30:16 +0100

    Add method to update horizontal/vertical lines

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -5,8 +5,8 @@ mod day5_test;
 
 #[derive(Debug, PartialEq)]
 struct Vent {
-    start : (u16, u16),
-    end : (u16, u16),
+    start : (usize, usize),
+    end : (usize, usize),
 }
 
 impl Vent {
@@ -14,19 +14,34 @@ impl Vent {
         let mut tuples = string.split(" -> ").to_owned();
         let mut start_iterator = tuples.next().unwrap().split(",");
         let start = (
-            start_iterator.next().unwrap().parse::<u16>().unwrap(),
-            start_iterator.next().unwrap().parse::<u16>().unwrap()
+            start_iterator.next().unwrap().parse::<usize>().unwrap(),
+            start_iterator.next().unwrap().parse::<usize>().unwrap()
         );
         let mut end_iterator = tuples.next().unwrap().split(",");
         let end = (
-            end_iterator.next().unwrap().parse::<u16>().unwrap(),
-            end_iterator.next().unwrap().parse::<u16>().unwrap()
+            end_iterator.next().unwrap().parse::<usize>().unwrap(),
+            end_iterator.next().unwrap().parse::<usize>().unwrap()
         );
 
         Vent {start, end}
     }
+
+    fn update_only_horizontal_and_vertical(&self, board : &mut [[u8 ; 1000] ; 1000]) {
+        if self.start.0 == self.end.0 {
+            let x = self.start.0;
+            for y in self.start.1 ..= self.end.1 {
+                board[x][y] += 1;
+            }
+        }
+        if self.start.1 == self.end.1 {
+            let y = self.start.1;
+            for x in self.start.0 ..= self.end.0 {
+                board[x][y] += 1;
+            }
+        }
+    }
 }
 
 fn main() {
-
+    
 }
--- src/bin/day5_test.rs
+++ src/bin/day5_test.rs
@@ -8,3 +8,27 @@ fn vent_reads_from_string_correctly() {
     let vent_2 = Vent::from_string("19,247 -> 999,0");
     assert_eq!(vent_2, Vent {start : (19, 247), end : (999, 0)});
 }
+
+#[test]
+fn vent_updates_horizontal_and_vertical_correctly() {
+    let vent = Vent::from_string("0,9 -> 5,9");
+    let mut array : [[u8; 1000] ; 1000] = [[0; 1000] ; 1000];
+
+    vent.update_only_horizontal_and_vertical(&mut array);
+
+    let mut manual_array : [[u8; 1000] ; 1000] = [[0; 1000] ; 1000];
+    for x in 0..=5 {
+        manual_array[x][9] = 1;
+    }
+
+    assert_eq!(array, manual_array);
+}
+
+#[test]
+fn vent_does_not_update_non_horizontal_vents() {
+    let vent = Vent::from_string("0,9 -> 5,0");
+    let mut array : [[u8; 1000] ; 1000] = [[0; 1000] ; 1000];
+    vent.update_only_horizontal_and_vertical(&mut array);
+
+    assert_eq!(array, [[0; 1000] ; 1000]);
+}

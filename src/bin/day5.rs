9f97b246d8efe0d5a53671ba3f6bcccd94c96a98
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2021-12-21 17:07:08 +0100

    Solve task 1

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,4 +1,5 @@
 use aoc_lib;
+use std::cmp;
 
 #[cfg(test)]
 mod day5_test;
@@ -29,19 +30,49 @@ impl Vent {
     fn update_only_horizontal_and_vertical(&self, board : &mut [[u8 ; 1000] ; 1000]) {
         if self.start.0 == self.end.0 {
             let x = self.start.0;
-            for y in self.start.1 ..= self.end.1 {
+            let min = cmp::min(self.start.1, self.end.1);
+            let max = cmp::max(self.start.1, self.end.1);
+            for y in min ..= max {
                 board[x][y] += 1;
             }
         }
-        if self.start.1 == self.end.1 {
+        else if self.start.1 == self.end.1 {
             let y = self.start.1;
-            for x in self.start.0 ..= self.end.0 {
+            let min = cmp::min(self.start.0, self.end.0);
+            let max = cmp::max(self.start.0, self.end.0);
+            for x in min ..= max {
                 board[x][y] += 1;
             }
         }
     }
 }
 
-fn main() {
+
+fn count(array : &[[u8; 1000] ; 1000]) -> usize {
+    array
+        .iter()
+        .flatten()
+        .filter(|&&val| val >= 2)
+        .count()
+}
+
+
+fn task1() {
+    println!("#####   TASK 1   #####");
+
+    let mut map : [[u8; 1000] ; 1000] = [[0; 1000] ; 1000];
+    for line in aoc_lib::read_entries("day5.txt").lines() {
+        let vent = Vent::from_string(line);
+        vent.update_only_horizontal_and_vertical(&mut map);
+    }
+
+    let map_count = count(&map);
     
+    println!("Points where two or more lines overlap: {}", map_count);
+}
+
+
+    
+fn main() {
+    task1();
 }
--- src/bin/day5_test.rs
+++ src/bin/day5_test.rs
@@ -10,7 +10,7 @@ fn vent_reads_from_string_correctly() {
 }
 
 #[test]
-fn vent_updates_horizontal_and_vertical_correctly() {
+fn vent_updates_horizontal_correctly() {
     let vent = Vent::from_string("0,9 -> 5,9");
     let mut array : [[u8; 1000] ; 1000] = [[0; 1000] ; 1000];
 
@@ -25,10 +25,60 @@ fn vent_updates_horizontal_and_vertical_correctly() {
 }
 
 #[test]
+fn vent_updates_vertical_correctly() {
+    let vent = Vent::from_string("5,0 -> 5,9");
+    let mut array : [[u8; 1000] ; 1000] = [[0; 1000] ; 1000];
+
+    vent.update_only_horizontal_and_vertical(&mut array);
+
+    let mut manual_array : [[u8; 1000] ; 1000] = [[0; 1000] ; 1000];
+    for y in 0..=9 {
+        manual_array[5][y] = 1;
+    }
+
+    assert_eq!(array, manual_array);
+}
+
+#[test]
 fn vent_does_not_update_non_horizontal_vents() {
     let vent = Vent::from_string("0,9 -> 5,0");
     let mut array : [[u8; 1000] ; 1000] = [[0; 1000] ; 1000];
     vent.update_only_horizontal_and_vertical(&mut array);
 
     assert_eq!(array, [[0; 1000] ; 1000]);
+}
+
+#[test]
+fn count_counts_correctly() {
+    let mut map : [[u8; 1000] ; 1000] = [[0; 1000] ; 1000];
+    map[0][0] = 2;
+    map[19][23] = 9;
+    map[22][31] = 100;
+    map[1][1] = 1;
+    map[99][99] = 1;
+    assert_eq!(count(&map), 3);
+}
+
+#[test]
+fn test_total_task_1() {
+    let inputs = 
+        "0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2";
+    let mut map : [[u8; 1000] ; 1000] = [[0; 1000] ; 1000];
+    for line in inputs.lines() {
+        let vent = Vent::from_string(line);
+        vent.update_only_horizontal_and_vertical(&mut map);
+    }
+
+    assert_eq!(count(&map), 5);
+}
+
 }

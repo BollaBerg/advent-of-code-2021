c2c258cca4accbaefcb463656cb58b9a7fc29d4e
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2022-01-05 18:32:38 +0100

    Solve task 1 of day 6

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,28 +1,29 @@
 use aoc_lib;
-use std::iter;
 
-const DAYS : u8 = 80;
-const NEW_FISH_AGE : u8 = 8;
-const BREED_AGE : u8 = 6;
+const DAYS : usize = 80;
+const NEW_FISH_AGE : usize = 8;
+const BREED_AGE : usize = 6;
 
 fn main() {
     let data = aoc_lib::read_entries("day6.txt");
     let entries = data
         .split(',')
-        .map(|v| v.parse::<u8>().unwrap());
+        .map(|v| v.parse::<usize>().unwrap());
     
-    let mut fish : Vec<u8> = entries.collect::<Vec<u8>>();
-    let mut to_add : u8;
+    let mut fish = [0; NEW_FISH_AGE + 1];
+    for fish_days in entries {
+        fish[fish_days] += 1;
+    }
     
-    for day in 0..DAYS {
-        to_add = 0;
-        fish = fish.iter().map(|v| {
-            if *v == 0 {to_add += 1; BREED_AGE} else {v - 1}
-        }).collect::<Vec<u8>>();
-
-        let fishes_to_add : Vec<u8> = iter::repeat(NEW_FISH_AGE).take(to_add.into()).collect();
-        fish.extend(fishes_to_add);
+    for _ in 0..DAYS {
+        let birthing_fish = fish[0];
+        for i in 0 .. fish.len() - 1 {
+            fish[i] = fish[i + 1];
+        }
+        fish[NEW_FISH_AGE] = birthing_fish;
+        fish[BREED_AGE] += birthing_fish;
     }
 
-    println!("Number of lanternfish after 80 days: {}", fish.len());
+    let number_of_fish : u32 = fish.iter().sum();
+    println!("Number of lanternfish after 80 days: {}", number_of_fish);
 }

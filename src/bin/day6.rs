2e796856bf5fbb0bf0e936e26439e850fa04922e
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2022-01-05 18:38:04 +0100

    Solve task 2 of day 6

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,8 +1,22 @@
 use aoc_lib;
 
-const DAYS : usize = 80;
 const NEW_FISH_AGE : usize = 8;
 const BREED_AGE : usize = 6;
+
+fn breed_fish(starting_fish : [u64; 9], days : usize) -> u64 {
+    let mut fish = starting_fish.clone();
+
+    for _ in 0..days {
+        let birthing_fish = fish[0];
+        for i in 0 .. fish.len() - 1 {
+            fish[i] = fish[i + 1];
+        }
+        fish[NEW_FISH_AGE] = birthing_fish;
+        fish[BREED_AGE] += birthing_fish;
+    }
+
+    fish.iter().sum()
+}
 
 fn main() {
     let data = aoc_lib::read_entries("day6.txt");
@@ -15,15 +29,9 @@ fn main() {
         fish[fish_days] += 1;
     }
     
-    for _ in 0..DAYS {
-        let birthing_fish = fish[0];
-        for i in 0 .. fish.len() - 1 {
-            fish[i] = fish[i + 1];
-        }
-        fish[NEW_FISH_AGE] = birthing_fish;
-        fish[BREED_AGE] += birthing_fish;
-    }
+    let number_of_fish_80 = breed_fish(fish, 80);
+    println!("Number of lanternfish after 80 days: {}", number_of_fish_80);
 
-    let number_of_fish : u32 = fish.iter().sum();
-    println!("Number of lanternfish after 80 days: {}", number_of_fish);
+    let number_of_fish_256 = breed_fish(fish, 256);
+    println!("Number of lanternfish after 256 days: {}", number_of_fish_256);
 }
